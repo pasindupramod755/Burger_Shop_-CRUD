9a4c5043268e9c1f99cb579a42738e5f8f545cc0
pasindupramod755 <pasidubandara12345@gmail.com>
2025-08-24 23:15:17 +0530

    Data save LocalStroge

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,25 @@
+const cartArray = [
+
+]
+
 function buyNow(id) {
-    let price = parseFloat(document.getElementById("price"+id.substring(3)).innerText.substring(3));
-    let image = document.getElementById("image"+id.substring(3)).src
+    let price = parseFloat(document.getElementById("price" + id.substring(3)).innerText.substring(3));
+    let image = document.getElementById("image" + id.substring(3)).src
+    let name = document.getElementById("name" + id.substring(3)).innerText
     console.log(id);
     console.log(price);
-    console.log(image);  
+    console.log(image);
+    const addNew = {
+        name,
+        price,
+        image
+    }
+    cartArray.push(addNew);
+    console.log(cartArray);
+    const stringifyArray = JSON.stringify(cartArray)
+    localStorage.setItem("dataArray", stringifyArray);
+    console.log(JSON.parse(localStorage.getItem("dataArray"))[0])
 }
+
+
+
